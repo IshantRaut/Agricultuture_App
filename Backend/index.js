086be77ec95b68c2dcfb50e7b3898103b5742284
IshantRaut <ishjee2018@gmail.com>
2025-06-03 13:58:03 +0530

    Cors Updated

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,10 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: process.env.CLIENT_URL || "*", // Set CLIENT_URL in your backend .env for more security
+  origin: [
+    "https://agricultuture-app-frontend.onrender.com",
+    "http://localhost:5173"
+  ],
   credentials: true,
 }));
 app.use(express.json({ limit: "5mb" }));
